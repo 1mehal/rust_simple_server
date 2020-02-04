e14139c0dd56d022a5e04c25538ef53750852545
1mehal <mehal.box@gmail.com>
2020-02-04 17:27:14 +0200

    - changed to use public key file in RS256 format

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use serde::{Serialize, Deserialize};
 
 use rocket::http::{Status};
 
-use jsonwebtoken::{decode,  Validation, DecodingKey};
+use jsonwebtoken::{decode,  Validation, DecodingKey, Algorithm};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Claims {
@@ -15,7 +15,9 @@ struct Claims {
 
 #[get("/hello?<token>")]
 fn hello(token: String) -> Result<String, Status> {
-    let _token_data = decode::<Claims>(&token, &DecodingKey::from_secret("kaoyjoAbPAlZdvXtenphsQnOd-rni5w3cvb3u_bWfvBoAMZLBeYs006gt5BPdQXZKBTiDQXwSIGeJSMEnaocPgpe9eSYOnDGR1cScC48E0Z-bpt6D9CbHRvf1rv4E9Z_d2lYlix3Yomaq198ap_dsICoqEffNH9aWl3sB_GLmS7yU7RehlTp1qye3Z-Y5FMnvlAUp0DqxcMDGoUAWYV_h-Rl_N8ZbB7YoH2MQ8N6N9S6rSUs6qnTqulmO7nT-Tbfg9-7FgZhahDuweEltC24lvkpTDEa5fQlmza4EqtCdi6tX39SNIOnj668w9_1fTJDJtsMx3OWzteine1yQc_KVQ".as_ref()), &Validation::default());
+    let pubkey_pem = include_bytes!("asdemo_jwt256.key.pub");
+    // let token = decode::<Claims>(&token, &DecodingKey::from_rsa_components(jwk["n"], jwk["e"]), &Validation::new(Algorithm::RS256))?;
+    let _token_data = decode::<Claims>(&token, &DecodingKey::from_rsa_pem(pubkey_pem).unwrap(), &Validation::new(Algorithm::RS256));
     match _token_data {
         Ok(c) => {
             return Ok(format!("Dear, {}, greetings from far far away rust server", c.claims.username));
